2588bb9e3f34f2bfd5b55ac9b63c478cc5945ada
romeerez <romadzao@gmail.com>
2022-12-04 05:23:55 -0500

    Check DATABASE_URL in prisma function

--- prisma/api/hello.ts
+++ prisma/api/hello.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
+const databaseURL = process.env.DATABASE_URL
+if (!databaseURL) {
+  throw new Error('DATABASE_URL is missing')
+}
+
 export default async function handler(request: VercelRequest, response: VercelResponse) {
   try {
     const users = await prisma.user.findMany();
